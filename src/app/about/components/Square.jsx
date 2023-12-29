e604e2edb6fb1123323efbc52ece3455b4d54d97
mmcN <mehmethilmi.temiz@gmail.com>
2023-12-29 20:41:47 +0300

    last

--- src/app/about/components/Square.jsx
+++ src/app/about/components/Square.jsx
@@ -7,7 +7,7 @@ class Square extends Component {
     render() { 
         return (
             <button className="but" disabled={this.props.clicked} onClick={() => this.props.onClick()}>
-			{this.props.value}  asd
+			{this.props.value}  
 		</button> 
         );
     }
